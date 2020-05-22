a130f7d024cfa5b37ce38cfca1fc5406bddd783d
[Divyanshu Chand s3767456] <s3767456@student.rmit.edu.au>
2020-05-22 19:38:08 +1000

    tute 10

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,8 +9,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     password: "foo",
     password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
     end
+    follow_redirect!
+    # assert_template 'users/show'
+    # assert is_logged_in?
+  end
 end
